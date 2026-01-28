80fe2f7a7d5c77a2c2362782b6d2b03b54012693
Matheus Machado <matheus.thosi@gmail.com>
2026-01-27 22:33:46 -0300

    validacao intervalo

--- adivinhar_numero.py
+++ adivinhar_numero.py
@@ -1,14 +1,22 @@
 import random
 from print_colorido import print_color
 
+INICIO = 1
+FINAL = 100
+
 if __name__ == "__main__":
     acertou = None
-    numero_escolha_bot = random.randint(1, 100)
+    numero_escolha_bot = random.randint(INICIO, FINAL)
 
     while not acertou:
         while True:
             try:
                 numero_escolha = int(input("\nTente adivinhar o número (1-100): "))
+
+                if not 1 <= numero_escolha <= 100:
+                    print_color("red", f"Número fora do intervalo! Digite um número entre 1 e 100.")
+                    continue
+
                 break
             except ValueError as e:
                 print_color("red", f"Entrada inválida: {e}")
